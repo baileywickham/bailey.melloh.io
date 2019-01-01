1163e67c0071e7e54522e1cabca0939abb4fbbf8
baileywickham <baileywickham@gmail.com>
2018-12-31 21:58:19 -0500

    added launch

---
+++ src/launch.sh
@@ -0,0 +1,3 @@
+sudo iptables -I INPUT -p tcp --dport 8000 ! -s 74.91.125.179 -j REJECT --reject-with tcp-reset
+sudo iptables -I INPUT -p tcp --dport 8081 ! -s 74.91.125.179 -j REJECT --reject-with tcp-reset
+tmux new -s rproxy 'exec ./docker-run.sh'
