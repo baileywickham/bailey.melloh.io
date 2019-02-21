218a3164c918ab502811df6ec0af17a0273f21b7
baileywickham <baileywickham@gmail.com>
2019-02-20 19:31:51 -0800

    rm outdated

--- outdated/mg.sh
+++
@@ -1,3 +0,0 @@
-mg="python mysite/manage.py makemigrations && python mysite/manage.py migrate"
-echo $mg | sudo docker run -i djangoproject_web cat -
-# do not use this script, it is unreliable at best.
--- outdated/mysite/fortune/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class FortuneConfig(AppConfig):
-    name = 'fortune'
-    
